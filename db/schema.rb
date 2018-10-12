8ab0176f0fd5d91190b32174f97f28e7d8cb0468
riku hatakeyama <happy.riku8888@gmail.com>
2018-10-12 11:34:07 +0900

    10/12 first

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2018_10_11_031609) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
